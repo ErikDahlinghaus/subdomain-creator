1d54b960744fbf8422aabf5096503e81b508d286
Erik Dahlinghaus <erik.dahlinghaus@gmail.com>
2013-06-07 23:23:13 -0400

    Initial commit... it works, horray!

---
+++ README.md
@@ -0,0 +1,7 @@
+subdom.sh
+======
+Automatically creates vhost and web root for a subdomain
+
+rev 0
+------
+-Semi-configurable options
---
+++ subdom.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+apache_sites_available='/etc/apache2/sites-available/'
+apache_www='/var/www/'
+skeleton_file='skel'
+
+domain='.dahlinghaus.net'
+
+
+# No need to edit below this point...
+if [ $# -eq 0 ]
+  then
+    echo -e "\nNo first argument..."
+    echo -e "Usage: $0 [-r] subdomain\n"
+    exit
+fi
+
+create(){
+	echo -e "\nCreating subdomin $subdir$domain"
+	echo -en "\t"
+	read -p "Is this correct? [y/n]" -n 1 -r
+	if [[ $REPLY =~ ^[Nn]$ ]]
+	  then
+	    echo -e "\nAborting...\n"
+	    exit
+	fi
+
+	echo -e '\nCreating vhosts file...'
+	while read LINE
+	do
+		echo $LINE | sed -e "s/!!!name!!!/$subdir/" >> /tmp/$subdir$domain
+	done < $apache_sites_available$skeleton_file$domain
+	sudo mv /tmp/$subdir$domain $apache_sites_available$subdir$domain
+
+
+	echo -e "Creating www subdirectory..."
+	mkdir -p $wwwdir
+
+	if [ -f $wwwdir"/index.html" ]
+	  then
+		echo "index.html already exists, not populating..."
+	  else
+		echo -e "Populating index.html..."
+		echo "<html><body><br /><br /><br /><center><h1>$subdir$domain</h1></center></body></html>" >> $wwwdir"/index.html"
+	fi
+
+	echo -e "Enabling site $subdir$domain..."
+	ERROR=$( sudo a2ensite $subdir$domain 2>&1 >/dev/null )
+	if [ "$ERROR" != "" ]
+	  then
+		echo -e "\t$ERROR"
+	fi
+
+	echo -e "Reloading apache config..."
+	ERROR=$( sudo service apache2 reload 2>&1 >/dev/null )
+	if [ "$ERROR" != "" ]
+	  then
+		echo -e "\t$ERROR"
+	fi
+}
+
+remove(){
+	echo -e "\nRemoving subdomin $subdir$domain"
+	echo -en "\t"
+	read -p "Is this correct? [y/n]" -n 1 -r
+	if [[ $REPLY =~ ^[Nn]$ ]]
+	  then
+	    echo -e "\nAborting...\n"
+	    exit
+  	fi
+	
+	echo -e '\nRemoving vhosts file...'
+	ERROR=$( sudo rm $apache_sites_available$subdir$domain 2>&1 >/dev/null )
+	if [ "$ERROR" != "" ]
+		  then
+		    echo -e "\t$ERROR"
+		fi
+
+	read -p "Remove www directory? [y/n]:" -n 1 -r
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	  then
+	    echo -e "\n\tRemoving www subdirectory..."
+	    ERROR=$( rm -r $wwwdir 2>&1 >/dev/null )
+	    if [ "$ERROR" != "" ]
+		  then
+		    echo -e "\t$ERROR"
+		fi
+	fi
+
+	echo -e "Disabling site $subdir$domain..."
+	ERROR=$( sudo a2dissite $subdir$domain 2>&1 >/dev/null )
+	if [ "$ERROR" != "" ]
+		  then
+		    echo -e "\t$ERROR"
+		fi
+
+	echo -e "Reloading apache config..."
+	ERROR=$( sudo service apache2 reload 2>&1 >/dev/null )
+	if [ "$ERROR" != "" ]
+		  then
+		    echo -e "\t$ERROR"
+		fi
+}
+
+if [ "$1" != "-r" ]
+  then
+    subdir=$1
+    wwwdir=$apache_www$subdir$domain
+    create
+  else
+    subdir=$2
+    wwwdir=$apache_www$subdir$domain
+    remove
+fi
+
+
+
+
+echo -e '\nDone.'
